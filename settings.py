aa424ad68a20233c73568b1e61924816f9c0f1f4
Dominic Miglar <dev@war10ck.netunix.org>
2011-09-26 22:03:37 +0200

    commit again

--- settings.py
+++ settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
-    'tinymce',
+    #####'tinymce',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
