7e60b8d268f02d5b36418ad17fb997de2e149d81
Dominic Miglar <dev@war10ck.netunix.org>
2011-09-28 18:30:45 +0200

    pushing again :D

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'web.views.news', name='news'),
-    url(r'^members$', 'web.views.members', name='members'),
+    url(r'^members/$', 'web.views.members', name='members'),
     url(r'^members/(?P<member_id>\d+)/$', 'web.views.memberDetail', name='memberdetails'),
     url(r'^sponsors/$', 'web.views.sponsors', name='sponsors'),
     url(r'^contact/$', 'web.views.contact', name='contact'),
